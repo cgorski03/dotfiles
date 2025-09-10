f53e65b6deccadea248af14df6a4faec08e9c27a
Colin Gorski <cgorski03@gmail.com>
2025-09-09 21:30:37 -0400

    fix: typescript lsp, cleaning up config

---
+++ nvim/lua/custom/plugins/treesitter.lua
@@ -0,0 +1,34 @@
+return {
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	opts = {
+		ensure_installed = {
+			"bash",
+			"c",
+			"diff",
+			"html",
+			"lua",
+			"luadoc",
+			"markdown",
+			"markdown_inline",
+			"query",
+			"vim",
+			"vimdoc",
+			"typescript",
+			"tsx",
+			"javascript",
+		},
+		auto_install = true,
+		highlight = {
+			enable = true,
+			additional_vim_regex_highlighting = { "ruby" },
+		},
+		indent = { enable = true, disable = { "ruby" } },
+	},
+	config = function(_, opts)
+		require("nvim-treesitter.configs").setup(opts)
+	end,
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter-textobjects",
+	},
+}
