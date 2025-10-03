e000d867b2e6f3ca8f6fa781d98409da9478a992
Colin Gorski <cgorski03@gmail.com>
2025-10-03 10:49:31 -0400

    fix: tabstop

--- nvim/init.lua
+++ nvim/init.lua
@@ -76,16 +76,11 @@ vim.api.nvim_create_autocmd({ "FocusGained", "WinEnter" }, {
 	end,
 })
 local js_grp = vim.api.nvim_create_augroup("Js2Spaces", { clear = true })
-vim.api.nvim_create_autocmd("FileType", {
-	group = js_grp,
-	pattern = { "javascript", "typescript", "javascriptreact", "typescriptreact" },
-	callback = function()
-		vim.opt_local.expandtab = true
-		vim.opt_local.tabstop = 2
-		vim.opt_local.shiftwidth = 2
-		vim.opt_local.softtabstop = 2
-	end,
-})
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true -- Convert tabs to spaces
+vim.opt.softtabstop = 2
+
 vim.api.nvim_create_autocmd({ "FocusLost", "WinLeave" }, {
 	pattern = "*",
 	callback = function()
